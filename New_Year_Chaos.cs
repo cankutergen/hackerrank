d0773285b979546f1689196d9b9654fe9b2cf81c
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 11:06:19 +0300

    Create New_Year_Chaos.cs

---
+++ New_Year_Chaos.cs
@@ -0,0 +1,15 @@
+    static void minimumBribes(int[] q) {
+            int ans = 0;
+            for (int i = q.Length - 1; i >= 0; i--)
+            {
+                if (q[i] - (i + 1) > 2)
+                {
+                    Console.WriteLine("Too chaotic");
+                    return;
+                }
+                for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+                    if (q[j] > q[i]) ans++;
+            }
+
+            Console.WriteLine(ans);
+    }
